d295a00c290d8421caf12c2e91b631b1b3d1ee6d
jolie_sky <jolie.mccullough@gmail.com>
2016-07-28 10:02:24 -0500

    added files for other stories, figuring out how webpack works

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,16 @@
-'use strict'
-/* global d3, stack */
+'use strict';
+
+import $ from 'jquery';
+import d3 from 'd3';
+
+/* global $, d3, stack */
+
 
 // var mystack = stack();
+$('.masthead-sections').mouseover(function() {
+  $('.navbar').show();
+});
+
+$('.navbar').mouseout(function() {
+  $('.navbar').hide();
+});
