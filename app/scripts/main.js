09683acb57caa72ee0a4af6e457344ba49472f70
jolie_sky <jolie.mccullough@gmail.com>
2016-08-05 17:57:53 -0500

    ben style tweaks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,12 +10,16 @@ import d3 from 'd3';
 $('.masthead-sections__desktop ul li').mouseenter(function() {
   $('.navbar--desktop .navbar__story').hide();
 
+  // $('.masthead').css({'border-bottom': '0', 'height': '2em'});
+
   $('.navbar--desktop #nav-' + this.id).show();
   $('.navbar--desktop').show();
 });
 
 $('.navbar--desktop').mouseleave(function() {
   $('.navbar--desktop').hide();
+
+  // $('.masthead').css({'border-bottom': '.5em solid white', 'height': '2.5em'});
 });
 
 $('.masthead-sections__mobile').click(function() {
