a4b2aa294b0b54fc9ef6c19b4b0fa470da80e6c5
jolie_sky <jolie.mccullough@gmail.com>
2016-08-11 12:22:46 -0500

    i guess i did some things

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,17 +10,15 @@ import d3 from 'd3';
 $('.masthead-sections__desktop ul li').mouseenter(function() {
   $('.navbar--desktop .navbar__story').hide();
 
-  // $('.masthead').css({'border-bottom': '0', 'height': '2em'});
-
   $('.navbar--desktop #nav-' + this.id).show();
   $('.navbar--desktop').show();
 });
 
-$('.navbar--desktop').mouseleave(function() {
-  $('.navbar--desktop').hide();
+//NEED TO HIDE THESE BASED ON RESIZE
 
-  // $('.masthead').css({'border-bottom': '.5em solid white', 'height': '2.5em'});
-});
+// $('.navbar--desktop').mouseleave(function() {
+//   $('.navbar--desktop').hide();
+// });
 
 $('.masthead-sections__mobile').click(function() {
   $('.navbar--mobile .navbar__story').toggle();
