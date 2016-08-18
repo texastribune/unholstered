3ba80743edb74b29fdc6b2e00da34f872631c5a9
jolie_sky <jolie.mccullough@gmail.com>
2016-08-17 19:55:30 -0500

    hide mobile nav if you switch to desktop

--- app/scripts/nav.js
+++ app/scripts/nav.js
@@ -1,5 +1,6 @@
 import $ from 'jquery'
 
+//opens navbar, sizes it and shows the right story
 $('.masthead-sections__desktop ul li').mouseenter(function () {
   $('.navbar--desktop .navbar__story').hide()
 
@@ -12,12 +13,27 @@ $('.masthead-sections__desktop ul li').mouseenter(function () {
 })
 
 //NEED TO HIDE ON RESIZE, SWITCH TO MOBILE
+//ALSO NEED TO FIX WEIRDNESS WITH HOVER IN MASTHEAD
 
+//closes navbar
 $('.navbar--desktop').mouseleave(function () {
   $('.navbar--desktop').hide()
+  //because i'm only leaving after leaving navbar, but if you never get into navbar it doesn't work. poop
 })
 
+//simple mobile nav on click
 $('.masthead-sections__mobile').click(function () {
   $('.navbar--mobile .navbar__story').toggle()
   $('.navbar--mobile').toggle()
+})
+
+//resize to switch from mobile to desktop nav if you resize
+$(window).resize(function() {
+  var windowWidth = $(window).width()
+  var mobilenav = $('.navbar--mobile').css('display') == 'block' ? true : false
+
+  if(windowWidth > 767 && mobilenav ) {
+    $('.navbar--mobile .navbar__story').toggle()
+    $('.navbar--mobile').toggle()
+  }
 })
