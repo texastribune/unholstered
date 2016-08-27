da345c9c11be595bb6f39c1b3c998e2c87295927
Ryan Murphy <rdmurphy@users.noreply.github.com>
2016-08-26 19:07:01 -0500

    Ben's red color change

--- app/scripts/BoxMaker.js
+++ app/scripts/BoxMaker.js
@@ -5,7 +5,7 @@ function BoxMaker (container) {
   let svg, g, xAxisG
   const isMobile = checkIfMobile()
 
-  const fillColor = 'rgb(209, 70, 33)'
+  const fillColor = 'rgba(255, 97, 74, 1)'
 
   const sizing = container.node().parentNode.getBoundingClientRect()
 
