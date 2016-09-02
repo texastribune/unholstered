3905c393f2b74d24b6410c5b230a96cc56e8cb53
Ryan Murphy <rdmurphy@users.noreply.github.com>
2016-09-02 13:38:30 -0500

    Do it live with new ad loader code

--- app/scripts/adLoader.js
+++ app/scripts/adLoader.js
@@ -1,64 +1,318 @@
-var googletag = googletag || {};
-googletag.cmd = googletag.cmd || [];
+'use strict'
 
-(function() {
-  var gads = document.createElement('script');
-  gads.async = true;
-  gads.type = 'text/javascript';
-  var useSSL = 'https:' === document.location.protocol;
-  gads.src = (useSSL ? 'https:' : 'http:') + '//www.googletagservices.com/tag/js/gpt.js';
-  var node = document.getElementsByTagName('script')[0];
-  node.parentNode.insertBefore(gads, node);
-})();
+Object.defineProperty(exports, '__esModule', { value: true })
 
-googletag.cmd.push(function() {
-  googletag.pubads().setTargeting('tribpedia', 'unholstered');
+/**
+ * Converts a string from dash or underscore separated to camelCase.
+ *
+ * @private
+ * @param  {String} str
+ * @return {String}
+ */
+function camelCase (str) {
+  // remove any dashes or underscores in the string, and grab the following character
+  return str.replace(/[\-_]+(\w?)/g, function (match, p1) {
+    // uppercase the following letter
+    return p1.toUpperCase()
+  })
+}
 
-  // BANNER SIZING
-  var bannerMapping = googletag.sizeMapping()
-    .addSize([0, 0], [300, 250])
-    .addSize([768, 1], [728, 90])
-    .build();
+/**
+ * Gets all the attributes set on an element, and returns them as an Object.
+ *
+ * @private
+ * @param  {Element} element
+ * @return {Object}
+ */
+function getAttributes (element) {
+  var attrs = {}
 
-  // INSIDE STORY SIZING
-  var insideMapping = googletag.sizeMapping()
-    .addSize([0, 0], [300, 250])
-    .addSize([510, 1], [468, 60])
-    .build();
+  // if element is empty, or not ELEMENT_NODE, eject
+  if (!element || element.nodeType !== 1) return attrs
 
-  // // 300X250 SIZING
-  // var sidebarMapping = googletag.sizeMapping()
-  //   .addSize([0, 0], [300, 250])
-  //   .build();
+  // grab all the attributes off the element
+  var map = element.attributes
 
+  // if there are no attributes, eject
+  if (map.length === 0) return attrs
 
-   // ALWAYS COMMENT OUT OR DELETE ADS YOU'RE NOT USING.
-   // CALLING ADS THAT AREN'T BEING DISPLAYED
-   // MAY CAUSE ALL OF THE AD SLOTS TO NOT "FETCH" OR "RENDER"
+  // loop through the attributes and build the object
+  for (var i = 0; i < map.length; i++) {
+    attrs[map[i].name] = map[i].value
+  }
 
-  // googletag.defineSlot('/5805113/TexasTribune_Site_Roofline1_ATF_Leaderboard_728x90', [728, 90], 'ad-banner-1')
-  //   .defineSizeMapping(bannerMapping)
-  //   .addService(googletag.pubads());
+  return attrs
+}
 
-  // googletag.defineSlot('/5805113/TexasTribune_Content_StoryLanding_ATF_RightRail1_MediumRectangle_300x250', [300, 250], 'ad-sidebar-1')
-  //   .defineSizeMapping(sidebarMapping)
-  //   .addService(googletag.pubads());
+/**
+ * Searches an element's attributes and returns an Object of all the ones that
+ * begin with a specified prefix. Each matching attribute name is returned
+ * without the prefix, with the remainder converted to camelCase.
+ *
+ * @private
+ * @param  {Element} element
+ * @param  {String} prefix
+ * @return {Object}
+ */
+function getMatchingAttributes (element, prefix) {
+  // get all the attributes on the element
+  var attributes = getAttributes(element)
+  var fields = {}
 
-  googletag.defineSlot('/5805113/TexasTribune_Content_StoryPage_InsideStory_468x60', [468, 60], 'ad-inside')
-    .defineSizeMapping(insideMapping)
-    .addService(googletag.pubads());
+  // loop through the keys of the attributes
+  Object.keys(attributes).forEach(function (key) {
+    // continue if the key begins with supplied prefix
+    if (key.substr(0, prefix.length) === prefix) {
+      // grab the value associated with the original key
+      var value = attributes[key]
 
-  googletag.defineSlot('/5805113/TexasTribune_Content_NewsApps_BTF_Footer_Leaderboard_728x90', [728, 90], 'ad-banner-footer')
-    .defineSizeMapping(bannerMapping)
-    .addService(googletag.pubads());
+      // slice off the prefix and camelCase what is left
+      var field = camelCase(key.slice(prefix.length))
 
-  googletag.pubads().enableSingleRequest();
-  googletag.enableServices();
-});
+      fields[field] = value
+    }
+  })
 
-googletag.cmd.push(function () {
-  // googletag.display('ad-banner-2');
-  // googletag.display('ad-sidebar-1');
-  googletag.display('ad-inside');
-  googletag.display('ad-banner-footer');
-});
+  return fields
+}
+
+/**
+ * A helper for asynchronously loading scripts.
+ *
+ * Provides a callback interface for passing a function that will only be called
+ * if the script is successfully added to the page.
+ *
+ * @private
+ * @param {Element} global Typically `window` - the browser context to work
+ * within
+ * @param {String} url The URL for the script to be loaded.
+ * @param {Function} [cb] A function to be called once the script successfully
+ * loads. Not required.
+ * @returns {void}
+ * @example
+ *
+ * loadScript('backup.js', function () {
+ *   // anything that depends on that script loading
+ * })
+ */
+function loadScript (global, url, cb) {
+  // create the `script` element
+  var script = global.document.createElement('script')
+
+  // set its URL
+  script.src = url
+
+  // if there is a supplied callback, add it to `onload`
+  if (cb) script.onload = cb
+
+  // attach the script to the document body
+  global.document.body.appendChild(script)
+}
+
+// Sourced from: https://github.com/sindresorhus/object-assign
+
+var hasOwnProperty = Object.prototype.hasOwnProperty
+var propIsEnumerable = Object.prototype.propertyIsEnumerable
+
+function toObject (val) {
+  if (val === null || val === undefined) {
+    throw new TypeError('Object.assign cannot be called with null or undefined')
+  }
+
+  return Object(val)
+}
+
+function shouldUseNative () {
+  try {
+    if (!Object.assign) {
+      return false
+    }
+
+    // Detect buggy property enumeration order in older V8 versions.
+
+    // https://bugs.chromium.org/p/v8/issues/detail?id=4118
+    var test1 = new String('abc'); // eslint-disable-line no-new-wrappers
+    test1[5] = 'de'
+    if (Object.getOwnPropertyNames(test1)[0] === '5') {
+      return false
+    }
+
+    // https://bugs.chromium.org/p/v8/issues/detail?id=3056
+    var test2 = {}
+    for (var i = 0; i < 10; i++) {
+      test2['_' + String.fromCharCode(i)] = i
+    }
+    var order2 = Object.getOwnPropertyNames(test2).map(function (n) {
+      return test2[n]
+    })
+    if (order2.join('') !== '0123456789') {
+      return false
+    }
+
+    // https://bugs.chromium.org/p/v8/issues/detail?id=3056
+    var test3 = {}
+    'abcdefghijklmnopqrst'.split('').forEach(function (letter) {
+      test3[letter] = letter
+    })
+    if (Object.keys(Object.assign({}, test3)).join('') !==
+      'abcdefghijklmnopqrst') {
+      return false
+    }
+
+    return true
+  } catch (err) {
+    // We don't expect any of the above to throw, but better to be safe.
+    return false
+  }
+}
+
+var objectAssign = shouldUseNative() ? Object.assign : function (target, source) {
+  var from
+  var to = toObject(target)
+  var symbols
+
+  for (var s = 1; s < arguments.length; s++) {
+    from = Object(arguments[s])
+
+    for (var key in from) {
+      if (hasOwnProperty.call(from, key)) {
+        to[key] = from[key]
+      }
+    }
+
+    if (Object.getOwnPropertySymbols) {
+      symbols = Object.getOwnPropertySymbols(from)
+      for (var i = 0; i < symbols.length; i++) {
+        if (propIsEnumerable.call(from, symbols[i])) {
+          to[symbols[i]] = from[symbols[i]]
+        }
+      }
+    }
+  }
+
+  return to
+}
+
+/**
+ * Counter used by {uniqueId} to iterate.
+ *
+ * @private
+ * @type {Number}
+ */
+var idCounter = 0
+
+/**
+ * Function for generating identifiers unique to a single browser session.
+ * Each time it runs, the integer at the end of the return value will increment.
+ *
+ * Borrowed from underscore.js: http://underscorejs.org/#uniqueId
+ *
+ * @param  {String} [prefix] Optional prefix for identifier
+ * @return {String}
+ * @example
+ * var identifier = uniqueId()
+ * // returns '1'
+ *
+ * var prefixedIdentifier = uniqueId('label-')
+ * // returns 'label-2'
+ */
+function uniqueId (prefix) {
+  var id = ++idCounter + ''
+  return prefix ? prefix + id : id
+}
+
+/**
+ * AdLoader
+ *
+ * @param  {Object} opts
+ * @return {Object}
+ */
+function AdLoader (opts) {
+  var w = window
+  var hasBeenInit = false
+  var adSlots
+
+  var defaultOpts = {
+    attributePrefix: 'dv-gpt-',
+    defaultAdFields: {
+      adUnit: '/5805113/basic',
+      dimensions: [300, 250]
+    },
+    globalMappings: {
+      banner: [
+        [[768, 130], [[728, 90]]]
+      ],
+      smallBanner: [
+        [[508, 100], [[468, 60]]]
+      ]
+    },
+    gptSrc: 'https://www.googletagservices.com/tag/js/gpt.js',
+    idPrefix: 'dv-gpt-',
+    selector: '.dv-gpt-ad',
+    targetingKey: 'tribpedia',
+    targetingValue: null
+  }
+
+  opts = objectAssign({}, defaultOpts, opts)
+
+  var adElements = Array.prototype.slice.call(document.querySelectorAll(opts.selector))
+
+  function setupService () {
+    w.googletag.cmd.push(function () {
+      var pubads = w.googletag.pubads()
+
+      if (opts.targetingKey && opts.targetingValue) {
+        pubads.setTargeting(opts.targetingKey, opts.targetingValue)
+      }
+
+      pubads.enableSingleRequest()
+      w.googletag.enableServices()
+    })
+  }
+
+  function createAds () {
+    adSlots = adElements.map(function (element) {
+      var matchingAttributes = getMatchingAttributes(element, opts.attributePrefix)
+      var localOpts = objectAssign({}, opts.defaultAdFields, matchingAttributes)
+
+      var adElementId = uniqueId(opts.idPrefix)
+      element.setAttribute('id', adElementId)
+
+      var adUnit = w.googletag.defineSlot(localOpts.adUnit, localOpts.dimensions, adElementId)
+
+      if (localOpts.mapping && opts.globalMappings.hasOwnProperty(localOpts.mapping)) {
+        adUnit.defineSizeMapping(opts.globalMappings[localOpts.mapping])
+      }
+
+      if (localOpts.targetingKey && localOpts.targetingValue) {
+        adUnit.setTargeting(localOpts.targetingKey, localOpts.targetingValue)
+      }
+
+      adUnit.setCollapseEmptyDiv(true)
+      adUnit.addService(w.googletag.pubads())
+
+      w.googletag.display(adElementId)
+
+      return adUnit
+    })
+  }
+
+  function init () {
+    if (hasBeenInit) return
+
+    loadScript(w, opts.gptSrc, function () {
+      w.googletag.cmd.push(setupService, createAds)
+
+      hasBeenInit = true
+    })
+  }
+
+  return {
+    adSlots: adSlots,
+    init: init
+  }
+}
+
+var dvAds = AdLoader() // singleton
+
+exports.AdLoader = dvAds
